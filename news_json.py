1fa6c5f7ecefd4d05b555ca990b39cc541292056
Bogorosh Evgeny <bogorosh@mail.ru>
2020-07-07 13:22:55 +0300

    First commit

---
+++ news_json.py
@@ -0,0 +1,48 @@
+def read_files(name):
+    'Читаем файл'
+    import json
+
+    with open(name, encoding='utf-8') as f:
+        json_data = json.load(f)
+        news_list = ''
+        for items in json_data['rss']['channel']['items']:
+            news_list += ' ' + items['description']
+        return news_list
+
+
+def count_word(news_list):
+    "Считаем слова длиннее 6 символов"
+    to_list = news_list.split(' ')
+    word_value = {}
+    for word in to_list:
+        if len(word) > 6:
+            if word in word_value:
+                word_value[word] += 1
+            else:
+                word_value[word] = 1
+    return word_value
+
+
+
+def sort_top(word_value):
+    'Сортируем и выводим ТОП10'
+    l = lambda word_value: word_value[1]
+    sort_list = sorted(word_value.items(), key=l, reverse=True)
+    count = 1
+    top_10 = {}
+    for word in sort_list:
+        top_10[count] = word
+        count += 1
+        if count == 10:
+            break
+    return top_10
+
+
+def main(name):
+    'главная функция: запрашивает имя файла, запускает другие функции'
+    top_10 = sort_top(count_word(read_files(name)))
+    for i in top_10.values():
+        print(i[1], ': ', i[0])
+
+
+main("newsafr.json")
